5bce518d00738b9b1f3971ae90982edd3782a256
Jaden <jespotten@hotmail.com>
2023-09-29 12:38:47 -0600

    Pipes

--- 04-lab-shell-pipeline/tsh.c
+++ 04-lab-shell-pipeline/tsh.c
@@ -109,53 +109,75 @@ void eval(char *cmdline)
     int stdout_len = sizeof(stdin_redir) / sizeof(stdin_redir[0]);
     int fd = -1;
     char *newenviron[] = { NULL };
+    int rdPipeFD = -1;
 
     parseline(cmdline, argv);
     int numCmds = parseargs(argv, cmds, stdin_redir, stdout_redir);
     builtin_cmd(argv);
 
-    
+    //fprintf(stderr, "%d\n", stdin_redir[0]);
     for(int i = 0; i < numCmds; i++)
     {
-        int pipefd[2];
-        int result = pipe(pipefd);
-
         int ret = fork();
         if(ret == 0)
         {
-            if(stdin_redir[0] > -1)
+            //fprintf(stderr, "HELLO\n");
+            if(i == 0)
             {
-                fd = open(argv[stdin_redir[0]], O_RDONLY);
-                printf("%d\n", fd);
-                dup2(fd, 0);
-                printf("%d\n", fileno(stdin));
-            }
-            //Changes 0 for stdout_len
-            if(stdout_redir[0] > -1)
+                if(stdin_redir[0] > -1)
+                {
+                    fd = open(argv[stdin_redir[0]], O_RDONLY);
+                    dup2(fd, 0);
+                }
+                if(numCmds > 1)
+                {
+                    int pipefd[2];
+                    int res = pipe(pipefd);
+                    dup2(pipefd[1], 1);
+                    close(pipefd[1]);  
+                    rdPipeFD = pipefd[0];
+                }
+            } 
+            else if(i == numCmds - 1)
             {
-                fd = open(argv[stdout_redir[0]], O_WRONLY | O_CREAT | O_TRUNC, 0600);
-                dup2(fd, 1);
-            }
-            if(fd != -1) close(fd);
-            if(numCmds > 1)
+                fprintf(stderr, "HELLO\n");
+                if(numCmds > 1)
+                {
+                    dup2(rdPipeFD, 0);
+                    close(rdPipeFD);
+                }
+                if(stdout_redir[stdout_len - 1] > -1)
+                {
+                    //fprintf(stderr, "%d\n", stdout_redir[stdout_len - 1]);
+                    fd = open(argv[stdout_redir[stdout_len - 1]], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+                    dup2(fd, 1);
+                }
+            } 
+            else
             {
+                int pipefd[2];
+                int res = pipe(pipefd);
                 dup2(pipefd[1], 1);
-                dup2(pipefd[0], 0);
                 close(pipefd[1]);
-                close(pipefd[0]);
-            }
-
+                dup2(rdPipeFD, 0);
+                close(rdPipeFD);
+                rdPipeFD = pipefd[0];
+            } 
+            if(fd != -1) close(fd);
             execve(argv[cmds[i]], &argv[cmds[i]] , newenviron);
         }
         else
         {
             setpgid(ret, ret);
             waitpid(ret, NULL, 0);
+            if(rdPipeFD != -1) close(rdPipeFD);
         }
-        fflush(stdin);
+        //fflush(stdin);
     }
     return;
 }
+
+
 
 // void handleReirection(int fd, char **argv, int *cmds, int *stdin_redir, int *stdout_redir)
 // {
