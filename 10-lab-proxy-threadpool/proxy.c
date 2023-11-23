41092a8ff3b5fd27441b9cfe33625016e53ca6e8
Jaden <jespotten@hotmail.com>
2023-11-23 12:33:50 -0700

    Part 4 Threadpool Complete

--- 10-lab-proxy-threadpool/proxy.c
+++ 10-lab-proxy-threadpool/proxy.c
@@ -8,6 +8,8 @@
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <semaphore.h>
 
 /* Recommended max object size */
 #define MAX_OBJECT_SIZE 102400
@@ -16,27 +18,44 @@
 
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0";
 
-struct args_struct {
-	int fd;
+typedef struct {
+    int *buf;          /* Buffer array */         
+    int n;             /* Maximum number of slots */
+    int front;         /* buf[(front+1)%n] is first item */
+    int rear;          /* buf[rear%n] is last item */
+    sem_t mutex;       /* Protects accesses to buf */
+    sem_t slots;       /* Counts available slots */
+    sem_t items;       /* Counts available items */
+} sbuf_t;
+
+sbuf_t sbuf;
+
+typedef struct {
+	int connfd;
 	struct sockaddr *local_addr;
 	socklen_t addr_len;
-};
+} args_struct;
 
-void *process_client(void*);
+args_struct args;
+
+void *process_clients(void*);
 int open_sfd(char *, struct sockaddr*, socklen_t);
 void handle_client(int, struct sockaddr*, socklen_t);
 int complete_request_received(char *);
 int parse_request(char *, char *, char *, char *, char *);
 void test_parser();
 void print_bytes(unsigned char *, int);
+void sbuf_init(sbuf_t *sp, int n);
+void sbuf_deinit(sbuf_t *sp);
+void sbuf_insert(sbuf_t *sp, int item);
+int sbuf_remove(sbuf_t *sp);
+
 
 
 int main(int argc, char *argv[])
 {
 	char *port = argv[1];
 	struct sockaddr_in ipv4addr;
-	struct args_struct args;
-	// struct sockaddr *local_addr;
 
 	ipv4addr.sin_family = AF_INET;
 	ipv4addr.sin_addr.s_addr = INADDR_ANY;
@@ -45,21 +64,30 @@ int main(int argc, char *argv[])
 	args.local_addr = (struct sockaddr *)&ipv4addr;
 
 	int sfd = open_sfd(port, args.local_addr, args.addr_len);
+
+	sbuf_init(&sbuf, SBUFSIZE);
+	pthread_t tid;
+	for(int i = 0; i < NTHREADS; i++)
+	{
+		pthread_create(&tid, NULL, process_clients, NULL);
+	}
+
 	while(1)
 	{
-		args.fd = accept(sfd, args.local_addr, &args.addr_len);
-		pthread_t tid;
-		pthread_create(&tid, NULL, &process_client, (void *)&args);
-		//handle_client(fd, local_addr, addr_len);
-	}
+		args.connfd = accept(sfd, args.local_addr, &args.addr_len);
+		sbuf_insert(&sbuf, args.connfd);
+	}		
 	return 0;
 }
 
-void *process_client(void *arguments)
+void *process_clients(void *arguments)
 {
-	struct args_struct *args = (struct args_struct *)arguments;
-	handle_client(args->fd, args->local_addr, args->addr_len);
 	pthread_detach(pthread_self());
+	while(1) 
+	{
+		int connfd = sbuf_remove(&sbuf);						/* Remove connfd from buffer */
+		handle_client(connfd, args.local_addr, args.addr_len);	/* Service client */
+	}
 	return NULL;
 }
 
@@ -219,6 +247,53 @@ int parse_request(char *request, char *method,
 		port[num_copy_bytes] = '\0';
 	}
 	return 1;
+}
+
+/* Create an empty, bounded, shared FIFO buffer with n slots */
+void sbuf_init(sbuf_t *sp, int n)
+{
+    sp->buf = calloc(n, sizeof(int)); 
+    sp->n = n;                       /* Buffer holds max of n items */
+    sp->front = sp->rear = 0;        /* Empty buffer iff front == rear */
+    sem_init(&sp->mutex, 0, 1);      /* Binary semaphore for locking */
+    sem_init(&sp->slots, 0, n);      /* Initially, buf has n empty slots */
+    sem_init(&sp->items, 0, 0);      /* Initially, buf has zero data items */
+}
+
+/* Clean up buffer sp */
+void sbuf_deinit(sbuf_t *sp)
+{
+    free(sp->buf);
+}
+
+/* Insert item onto the rear of shared buffer sp */
+void sbuf_insert(sbuf_t *sp, int item)
+{
+    printf("before wait on slots\n"); fflush(stdout);
+    sem_wait(&sp->slots);                          /* Wait for available slot */
+    printf("after wait on slots\n"); fflush(stdout);
+    sem_wait(&sp->mutex);                          /* Lock the buffer */
+    sp->buf[(++sp->rear)%(sp->n)] = item;   /* Insert the item */
+    sem_post(&sp->mutex);                          /* Unlock the buffer */
+    printf("before posting items\n"); fflush(stdout);
+    sem_post(&sp->items);                          /* Announce available item */
+    printf("after posting items\n"); fflush(stdout);
+}
+
+/* Remove and return the first item from buffer sp */
+int sbuf_remove(sbuf_t *sp)
+{
+    int item;
+    printf("before wait on items\n"); fflush(stdout);
+    sem_wait(&sp->items);                          /* Wait for available item */
+    printf("after wait on items\n"); fflush(stdout);
+    sem_wait(&sp->mutex);                          /* Lock the buffer */
+    item = sp->buf[(++sp->front)%(sp->n)];  /* Remove the item */
+    sem_post(&sp->mutex);                          /* Unlock the buffer */
+    printf("before posting slots\n"); fflush(stdout);
+    sem_post(&sp->slots);                          /* Announce available slot */
+    printf("after posting slots\n"); fflush(stdout);
+    return item;
 }
 
 
